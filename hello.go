bab68f6934d5f0c15ad63e0b6475298c3e3d4d93
siddharthzs <siddharthchoudhary.mail@gmail.com>
2021-11-25 01:48:15 +0530

    pointer and deference complete

--- hello.go
+++ hello.go
@@ -5,7 +5,14 @@ import (
 	"fmt"
 	// "os"
 	// "strconv"
+	"math"
 )
+
+
+type Point struct{
+	x float64
+	y float64
+}
 
 func main() {
 	var language string = "Go"
@@ -113,8 +120,32 @@ func main() {
 
 
 	// Array are immutable
+
+	// Pointedrs and Deference
+	// & *
+	tochange := "hello"
+	fmt.Println(tochange)
+
+
+	
 }
 
+
+func distance(p1 Point, p2 Point) float64 {
+	return math.Sqrt(math.Pow(p1.x-p2.x,2) + math.Pow(p1.y-p2.y,2))
+}
+
+
+
+
+
+
+func changeval(str string){
+	str = "Changed!"
+}
+func changePointer(str *string){
+	*str = "Changed!"
+}
 
  func apsolute(x int) int {
 	 defer fmt.Println("run absolute for ",x)
